b961212238a31f253885eee944df07914bd8b80d
Gockster <gm.dimitriadis@hotmail.com>
2020-10-06 23:57:59 +0300

    Project setup

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/george/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,31 @@
+package com.george.ecommerce.config;
+
+import com.george.ecommerce.entity.Product;
+import com.george.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST,HttpMethod.DELETE};
+
+
+        //disable http methods for product: put, post, delete
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        //disable http methods for product category: put, post, delete
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
---
+++ 03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: string;
+    imageYrl: string;
+    active: string;
+    unitsInStock: string;
+    dateCreated: Date;
+    lastUpdated: Date;
+}
